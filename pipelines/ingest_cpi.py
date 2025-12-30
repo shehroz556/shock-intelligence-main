3b6372bdac63819463b67144b3273d80dc9850a9
Shehroz Jawad Khan <shehroz556@gmail.com>
2025-12-30 13:26:19 +0500

    Ingest CPI Monthly Data

---
+++ pipelines/ingest_cpi.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from db_connection import get_connection
+
+CSV_PATH = r"C:\Users\Shehroz\Documents\shock-intelligence-main\data_raw\cpi_mom_pakistan.csv"
+
+def ingest_cpi():
+    df = pd.read_csv(CSV_PATH)
+    df.columns = [c.lower().strip() for c in df.columns]
+
+    df = df[["date", "value"]]
+    df["country"] = "Pakistan"
+    df["indicator"] = "CPI"
+    df["source"] = "PBS"
+    df["frequency"] = "Monthly"
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    for _, row in df.iterrows():
+        cur.execute(
+            """
+            INSERT INTO time_series
+            (country, indicator, date, value, source, frequency)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            ON CONFLICT DO NOTHING;
+            """,
+            (
+                row["country"],
+                row["indicator"],
+                row["date"],
+                row["value"],
+                row["source"],
+                row["frequency"]
+            )
+        )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print("✅ CPI ingestion completed successfully")
+
+if __name__ == "__main__":
+    ingest_cpi()
